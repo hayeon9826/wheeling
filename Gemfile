99d96360c32264ffe96132ff2eef57e070de609b
Hayeon <hayeon226@likelion.org>
2019-04-02 01:38:48 +0900

    admin, image uploader

--- Gemfile
+++ Gemfile
@@ -3,7 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.4.2'
 
-
+gem 'carrierwave', '~> 1.0'
+gem 'activeadmin'
 gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2', '>= 5.2.2.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
   get 'home/index'
   root 'home#index'
   resources :posts
+  get 'location/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
